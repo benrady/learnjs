93e6e9b4e704fb343e1eca4350c42b28f87cf46a
Ben Rady <benrady@gmail.com>
2015-12-15 12:46:02 -0600

    Added sspa action to build and deploy services

--- services/lib/deploy.js
+++ services/lib/deploy.js
@@ -1,11 +1,10 @@
 var AWS = require('aws-sdk');
 var fs = require('fs');
-var config = require('../conf/' + process.argv[2] + '.js');
 
 AWS.config.region = 'us-east-1'
 var lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
 var params = {
-  FunctionName: config.functionName, 
+  FunctionName: 'learnjs_popularAnswers',
   ZipFile: fs.readFileSync('archive.zip')
 };
 lambda.updateFunctionCode(params, function(err, data) { 
