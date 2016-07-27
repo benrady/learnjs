10d2cc94296e3b7c67e0036be85ca88167c4b9fb
Li-Ting Tsai <lt.tsai@hopebaytech.com>
2016-07-27 14:39:12 +0800

    add cljs framewrok

---
+++ src/frontend/core.cljs
@@ -0,0 +1,26 @@
+(ns frontend.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+(println "This text is printed from src/frontend/core.cljs. Go ahead and edit it and see reloading in action.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+(om/root
+  (fn [data owner]
+    (reify om/IRender
+      (render [_]
+        (dom/h1 nil (:text data)))))
+  app-state
+  {:target (. js/document (getElementById "app"))})
+
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
