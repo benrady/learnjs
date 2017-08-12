e33afb82764a21b0f7d398037c2e4429e5d2f41f
Yutaka Hiroyama <hiroyama@mbk.nifty.com>
2017-08-12 12:46:35 +0900

    p.38 2.3 finished

---
+++ public/tests/app_spec.js
@@ -0,0 +1,10 @@
+describe('LearnJS', function() {
+  it('can show a problem view', function() {
+    learnjs.showView('#problem-1');
+      expect($('.view-container .problem-view').length).toEqual(1);
+  });
+  it('show the landing page view when there is no hash', function(){
+    learnjs.showView('');
+    expect($('.view-container .landing-view').length).toEqual(1);
+  });
+});
