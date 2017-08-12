1274a5d8c2385ab50ef81246a9cddbcf7a435576
Yutaka Hiroyama <hiroyama@mbk.nifty.com>
2017-08-12 13:52:46 +0900

    Chapter 2nd finished.

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -7,4 +7,33 @@ describe('LearnJS', function() {
     learnjs.showView('');
     expect($('.view-container .landing-view').length).toEqual(1);
   });
+  it('passes the hash view parameter to the view function', function() {
+    spyOn(learnjs, 'problemView');
+    learnjs.showView('#problem-42');
+    expect(learnjs.problemView).toHaveBeenCalledWith('42');
+  });
+
+  it('invokes the router when loaded', function() {
+    spyOn(learnjs, 'showView');
+    learnjs.appOnReady();
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  });
+  it('subscribes to the hash change event', function() {
+    learnjs.appOnReady();
+    spyOn(learnjs, 'showView');
+    $(window).trigger('hashchange');
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  });
+/*
+  it('', function() {
+    expect();
+  });
+*/
+
+  describe('problem view', function() {
+    it('has a title that includes the problem number', function() {
+      var view = learnjs.problemView('1');
+      expect(view.text()).toEqual('Problem #1 Coming soon!');
+    });
+  });
 });
