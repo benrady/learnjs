eebe96bcb56508351b071b813a3cb5e7e860563e
Shinogasa <cuulbulu@gmail.com>
2017-09-27 21:58:15 +0900

    firstcommit spa1

---
+++ public/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var learnjs = {};
+
+learnjs.problemView = function() {
+    return $('<div class="problem-view">').text('Coming soon!');
+}
+
+learnjs.showView = function(hash) {
+    var routes = {
+        '#problem-1': learnjs.problemView
+    };
+    var viewFn = routes[hash];
+    if (viewFn) {
+        $('.view-container').empty().append(viewFn());
+    }
+}
---
+++ public/tests/app_spec.js
@@ -0,0 +1,11 @@
+describe('LearnJS', function() {
+    it('can show a ploblem view', function(){
+        learnjs.showView('#problem-1');
+        expect($('.view-container .problem-view').length).toEqual(1);
+    });
+
+    it('shows the landing page view when there is no hash', function(){
+        learnjs.showView('');
+        expect($('.view-container .landing-view').length).toEqual(1);
+    });
+});
