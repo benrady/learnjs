d6d8a5e7bbf9f52f9828adc0a9fe35f9c15f7b31
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-11-17 12:45:25 +0900

    app.js

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      "public/app.js",
       "public/tests/lib/*.js",
       "public/tests/app_spec.js",
       "public/tests/SpecHelper.js"
