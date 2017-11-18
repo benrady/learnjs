95307a047fd5f3bd507241de525e793c2ee6b65a
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-11-18 10:37:39 +0900

    ルーターを定義

--- public/app.js
+++ public/app.js
@@ -1,7 +1,18 @@
 'use strict';
 var learnjs = {};
 
+learnjs.problemView = function() {
+    return $('<div class="problem-view">').text('Coming soon!');
+}
+
 learnjs.showView = function(hash) {
-    var problemView = $('<div class="problem-view">').text('Coming soon!');
-    $('.view-container').empty().append(problemView);
+    var routes = {
+        '#problem-1': learnjs.problemView
+    };
+    var viewFn = routes[hash]
+    if (viewFn) {
+        $('.view-container').empty().append(viewFn());
+    }
+    
+    
 }
