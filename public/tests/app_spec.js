6bb663b838db4f21774713d709e96fa6628fc762
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-11-18 12:39:51 +0900

    テスト修正

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -26,9 +26,9 @@ describe('LearnJS', function() {
     });
 
     describe('problem view', function() {
-        it('has a little that includes the problem number', function() {
+        it('has a title that includes the problem number', function() {
             var view = learnjs.problemView('1');
-            expect(view.text()).toEqual('Problem #1 Coming Soon!');
+            expect(view.find('.title').text()).toEqual('Problem #1');
         }); 
     });
 });
