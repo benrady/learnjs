5439b80010f365ecf4e5c1c04abedf6d43c33a67
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-11-18 11:03:28 +0900

    view parameterをとれるように変更

--- public/app.js
+++ public/app.js
@@ -1,17 +1,19 @@
 'use strict';
 var learnjs = {};
 
-learnjs.problemView = function() {
-    return $('<div class="problem-view">').text('Coming soon!');
+learnjs.problemView = function(problemNumber) {
+    var title = 'Problem #' + problemNumber + ' Coming Soon!';
+    return $('<div class="problem-view">').text(title);
 }
 
 learnjs.showView = function(hash) {
     var routes = {
-        '#problem-1': learnjs.problemView
+        '#problem': learnjs.problemView
     };
-    var viewFn = routes[hash]
+    var hashParts = hash.split('-');
+    var viewFn = routes[hashParts[0]] // #problem -> learnjs.problemView
     if (viewFn) {
-        $('.view-container').empty().append(viewFn());
+        $('.view-container').empty().append(viewFn(hashParts[1]));
     }
     
     
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -7,5 +7,17 @@ describe('LearnJS', function() {
         learnjs.showView('');
         expect($('.view-container .landing-view').length).toEqual(1);
     });
+    it('passes the hash view parameter to the view function', function() {
+        spyOn(learnjs, 'problemView');
+        learnjs.showView('#problem-42');
+        expect(learnjs.problemView).toHaveBeenCalledWith('42');
+    });
+
+    describe('problem view', function() {
+        it('has a little that includes the problem number', function() {
+            var view = learnjs.problemView('1');
+            expect(view.text()).toEqual('Problem #1 Coming Soon!');
+        }); 
+    });
 });
 
