8e39bb4e1874f21510d2ad7bbd7a88a321ac2d28
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-12-06 22:13:41 +0900

    webpack,bable導入してES2015で書けるようにした

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+module.exports = {
+    // context: __dirname + '/public',
+    entry: {
+        'app': './src/app.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'public/js'),
+      filename: '[name].js'
+    },
+    module: {
+      loaders: [
+        { 
+          test: /\.js$/, 
+          exclude: /node_modules/, 
+          loader: "babel-loader", 
+          query:{
+            presets: ['react', 'es2015']
+          }
+        }
+      ]
+    }
+  };
+
+// const path = require('path');
+
+// module.exports = {
+//     entry: './public/app-test.js',
+//     output: {
+//         path: path.resolve(__dirname, 'bin'),
+//         filename: 'app.bundle.js',
+//     },
+//     module: {
+//         loaders: [{
+//             test: /\.js$/,
+//             exclude: /node_modules/,
+//             loader: 'e'
+//         }]
+//     }
+// }
