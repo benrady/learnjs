0d2600a49837f8bf9bb2ec717f1aae804a2d1835
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-12-08 02:00:32 +0900

    reactやめる

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
           exclude: /node_modules/, 
           loader: "babel-loader", 
           query:{
-            presets: ['react', 'es2015']
+            presets: ['es2015']
           }
         }
       ]
