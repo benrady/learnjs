39a63da4b1b3dd126c62b3fa899b18a8ff2ac2d2
Shigeto Moriyasu <leon@328w.co.jp>
2018-01-04 21:54:34 +0900

    ルート追加

--- public/app.js
+++ public/app.js
@@ -1,6 +1,16 @@
 'use strict'
 let learnjs = {}
+
+learnjs.problemView = () => {
+  return $('<div class="problem-view">').text('Coming soon!');
+}
+
 learnjs.showView = (hash) => {
-  let problemView = $('<div class="problem-view">').text('Coming soon!');
-  $('.view-container').empty().append(problemView);
+  let routes = {
+    '#problem-1': learnjs.problemView
+  }
+  let viewFn = routes[hash];
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn());
+  }
 }
