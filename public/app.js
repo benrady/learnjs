bc11f6e7b004274887bd55711a8c008c28cb970f
Shigeto Moriyasu <leon@328w.co.jp>
2018-01-04 20:54:05 +0900

    ルータ関数showViewを追加

--- public/app.js
+++ public/app.js
@@ -1,2 +1,6 @@
 'use strict'
 let learnjs = {}
+learnjs.showView = (hash) => {
+  let problemView = $('<div class="problem-view">').text('Coming soon!');
+  $('.view-container').empty().append(problemView);
+}
