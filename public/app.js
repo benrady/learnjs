ad205cae13efc848ba6d0546a2c3bbd15292c537
Shigeto Moriyasu <leon@328w.co.jp>
2018-01-04 22:05:47 +0900

    ビューパラメータを追加

--- public/app.js
+++ public/app.js
@@ -7,10 +7,11 @@ learnjs.problemView = () => {
 
 learnjs.showView = (hash) => {
   let routes = {
-    '#problem-1': learnjs.problemView
+    '#problem': learnjs.problemView
   }
-  let viewFn = routes[hash];
+  let hashParts = hash.split('-');
+  let viewFn = routes[hashParts[0]];
   if (viewFn) {
-    $('.view-container').empty().append(viewFn());
+    $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 }
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -7,5 +7,10 @@ describe('LearnJS', function() {
     learnjs.showView('');
     expect($('.view-container .landing-view').length).toEqual(1);
   });
-})
+  it('passes the hash view parameter to the view function', () => {
+    spyOn(learnjs, 'problemView');
+    learnjs.showView('#problem-42');
+    expect(learnjs.problemView).toHaveBeenCalledWith('42');
+  });
+});
 
