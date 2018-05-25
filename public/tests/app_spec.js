4b4ef4a19c993896138752eb868a42e216d2b809
kouestu_serizawa <kouetsu_serizawa@mediado.jp>
2018-05-25 20:10:37 +0900

    今日も頑張った

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,4 +1,8 @@
 describe('LearnJS', function(){
+    it('shows the landing page view when there is no hash', function(){
+        learnjs.showView('');
+        expect($('.view-container .landing-view').length).toEqual(1);
+    });
     it('can show a problem view', function(){
         learnjs.showView('#problem-1');
         expect($('.view-container .problem-view').length).toEqual(1);
