f40c90d1e53e7301886406413115ca0db7bedb8b
Justice <develop-man@yutori-tech.com>
2018-08-22 23:14:37 +0900

    Implemented routing

--- public/app.js
+++ public/app.js
@@ -1,8 +1,29 @@
 'use strict'
 var learnjs = {}
+
+learnjs.appOnReady = function() {
+  window.onhashchange = function() {
+    learnjs.showView(window.location.hash)
+  }
+  learnjs.showView(window.location.hash)
+}
+
+learnjs.problemView = function(problemNumber) {
+  var title = 'Problem #' + problemNumber + ' Coming soon!'
+  console.log(title)
+  return $('<div class="problem-view">').text(title)
+}
+
 learnjs.showView = function(hash) {
-  var problemView = $('<div class="problem-view">').text('Coming soon')
-  $('.view-container')
-    .empty()
-    .append(problemView)
+  var routes = {
+    '#problem': learnjs.problemView
+  }
+  var hashParts = hash.split('-')
+  var viewFn = routes[hashParts[0]]
+
+  if (viewFn) {
+    $('.view-container')
+      .empty()
+      .append(viewFn(hashParts[1]))
+  }
 }
