9183ec75e515df53031608131a67d671bc82ed96
acio-o9 <aco6280113@gmail.com>
2020-11-15 09:14:52 +0900

    [add] problem view test

---
+++ public/tests/app_spec.js
@@ -0,0 +1,6 @@
+describe('LearnJS', function() {
+  it('can show a problem view', function() {
+    learnjs.showView('#problem-1');
+    expect($('.view-container .problem-view').length).toEqual(1);
+  });
+});
