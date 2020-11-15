9be48bf030d74567638b34c581b025c83698d399
acio-o9 <aco6280113@gmail.com>
2020-11-15 09:38:55 +0900

    [add] show #problem-1

---
+++ public/app.js
@@ -0,0 +1,6 @@
+'use strict';
+var learnjs = {};
+learnjs.showView = function() {
+  var problemView = $('<div class="problem-view">').text('Coming soon!');
+  $('.view-container').empty().append(problemView);
+}
