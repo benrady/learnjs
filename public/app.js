88d5846050aa4e9afc557a4d69b85ecb2af4a623
acio-o9 <aco6280113@gmail.com>
2020-11-15 11:56:05 +0900

    [modify] problem view function use problemNumber

--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
 'use strict';
 var learnjs = {};
 
-learnjs.problemView = function() {
-  return $('<div class="problem-view">').text('Coming soon!');
+learnjs.problemView = function(problemNumber) {
+  var title = 'Problem #' + problemNumber + ' Coming soon!';
+  return $('<div class="problem-view">').text(title);
 }
 
 learnjs.showView = function(hash) {
