e6c6c7ea7850189877f4caf971cdfbafd84526cc
acio-o9 <aco6280113@gmail.com>
2020-11-15 09:45:22 +0900

    [add] tests nullcase

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -3,4 +3,8 @@ describe('LearnJS', function() {
     learnjs.showView('#problem-1');
     expect($('.view-container .problem-view').length).toEqual(1);
   });
+  it('shows the landing page view when there is no hash', function() {
+    learnjs.showView('');
+    expect($('.view-container .landing-view').length).toEqual(1);
+  });
 });
