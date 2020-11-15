63e464187cef220805fb630fb7a57b58f835b7b5
acio-o9 <aco6280113@gmail.com>
2020-11-15 11:21:20 +0900

    [add] show landingpage

--- public/app.js
+++ public/app.js
@@ -1,6 +1,16 @@
 'use strict';
 var learnjs = {};
-learnjs.showView = function() {
-  var problemView = $('<div class="problem-view">').text('Coming soon!');
-  $('.view-container').empty().append(problemView);
+
+learnjs.problemView = function() {
+  return $('<div class="problem-view">').text('Coming soon!');
+}
+
+learnjs.showView = function(hash) {
+  var routes = {
+    '#problem-1': learnjs.problemView
+  };
+  var viewFn = routes[hash];
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn());
+  }
 }
