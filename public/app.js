9393469cd62e3993fbdea33c37856dde68f95673
acio-o9 <aco6280113@gmail.com>
2020-11-17 07:53:44 +0900

    [add] check answer

--- public/app.js
+++ public/app.js
@@ -15,8 +15,27 @@ learnjs.problems = [
 learnjs.problemView = function(data) {
   var problemNumber = parseInt(data, 10);
   var view = $('.templates .problem-view').clone();
+  var problemData = learnjs.problems[problemNumber - 1];
+  var resultFlash = view.find('.result');
+
+  function checkAnswer() {
+    var answer = view.find('.answer').val();
+    var test = problemData.code.replace('__', answer) + '; problem();';
+    return eval(test);
+  }
+
+  function checkAnswerClick() {
+    if (checkAnswer()) {
+      resultFlash.text('Correct!');
+    } else {
+      resultFlash.text('Incorrect!');
+    }
+    return false;
+  }
+
+  view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
-  learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
+  learnjs.applyObject(problemData, view);
   return view;
 }
 
