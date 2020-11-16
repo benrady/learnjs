95ab861583c313233afd4d5b42bf5d49ecd81b52
acio-o9 <aco6280113@gmail.com>
2020-11-17 07:38:58 +0900

    [add] answer check test

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -40,3 +40,20 @@ describe('problem view', function() {
     expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
   })
 });
+
+describe('answer section', function() {
+  var view;
+  beforeEach(function() {
+    view = learnjs.problemView('1');
+  });
+  it('can check a correct answer by hitting a button', function() {
+    view.find('.answer').val('true');
+    view.find('.check-btn').click();
+    expect(view.find('.result').text()).toEqual('Correct!');
+  });
+  it('rejects an incorrect answer', function() {
+    view.find('.answer').val('true');
+    view.find('.check-btn').click();
+    expect(view.find('.result').text()).toEqual('Incorrect!');
+  });
+});
