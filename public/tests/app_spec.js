fd7957b540113b277653a02ea0c28920b7f3a2c0
acio-o9 <aco6280113@gmail.com>
2020-11-17 07:36:07 +0900

    [refact] view initialize on beforeEach

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -26,16 +26,17 @@ describe('LearnJS', function() {
 });
 
 describe('problem view', function() {
+  var view;
+  beforeEach(function() {
+    view = learnjs.problemView('1');
+  });
   it('has a title that includes the problem number', function() {
-    var view = learnjs.problemView('1');
     expect(view.find('.title').text()).toEqual('Problem #1');
   });
   it('shows the description', function() {
-    var view = learnjs.problemView('1');
     expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
   })
   it('shows the problem code', function() {
-    var view = learnjs.problemView('1');
     expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
   })
 });
