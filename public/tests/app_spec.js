6ceb63781fc6b21f43e1f38e0ab449994733c8cf
acio-o9 <aco6280113@gmail.com>
2020-11-23 14:50:18 +0900

    fixup! [add] show next problem when you solved

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -49,7 +49,7 @@ describe('answer section', function() {
   it('can check a correct answer by hitting a button', function() {
     view.find('.answer').val('true');
     view.find('.check-btn').click();
-    expect(view.find('.result').text()).toEqual('Correct!');
+    expect(view.find('a').attr('href')).toEqual('#problem-2');
   });
   it('rejects an incorrect answer', function() {
     view.find('.answer').val('false');
