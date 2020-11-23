84350b4680efe7be89469f012b5876e3ef89ed53
acio-o9 <aco6280113@gmail.com>
2020-11-23 15:08:08 +0900

    [add] top-fixed tool bar

--- public/app.js
+++ public/app.js
@@ -57,6 +57,7 @@ learnjs.applyObject = function(obj, elem) {
 learnjs.showView = function(hash) {
   var routes = {
     '#problem': learnjs.problemView,
+    '#': learnjs.landingView,
     '': learnjs.landingView
   };
   var hashParts = hash.split('-');
